0a9c754183a7e132c3d014ebb962c389f51e8ddb
김용원 <qwsa522@naver.com>
2024-09-02 13:05:40 +0900

    feat : BE 초기환경세팅 #44

---
+++ backend/src/main/java/com/ddubucks/readygreen/ReadygreenApplication.java
@@ -0,0 +1,13 @@
+package com.ddubucks.readygreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadygreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadygreenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ddubucks/readygreen/ReadygreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddubucks.readygreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadygreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
