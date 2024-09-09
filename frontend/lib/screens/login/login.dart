92911c696e3257b0aed35fcd7ea754b88671361d
이승민 <tmdals77117@naver.com>
2024-09-09 17:07:54 +0900

    feat: screens 구조

---
+++ frontend/lib/screens/login/login.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:readygreen/screens/main/main_page.dart'; // 메인 페이지를 임포트
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("로그인")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: '아이디',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: '비밀번호',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // 로그인 성공 시 메인 페이지로 이동
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => MainPage()));
+              },
+              child: Text("로그인"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/screens/main/main_page.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:readygreen/widgets/common/button.dart';
+import 'package:readygreen/widgets/common/cardbox.dart';
+import 'package:readygreen/widgets/common/squarecardbox.dart';
+import 'package:readygreen/bottom_navigation.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  List<Widget> _pages = [
+    const MainScreenContent(),
+    Center(child: Text('검색 화면', style: TextStyle(fontSize: 24))),
+    Center(child: Text('설정 화면', style: TextStyle(fontSize: 24))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F9),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigation(),
+    );
+  }
+}
+
+class MainScreenContent extends StatelessWidget {
+  const MainScreenContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 35,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '언제그린',
+                      style: TextStyle(
+                        color: Color(0xFF7FC818),
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SquareCardBox(
+                  title: '날씨',
+                ),
+                SquareCardBox(
+                  title: '오늘의 운세',
+                  backgroundColor: Color(0xFF2D3765),
+                  textColor: Colors.white,
+                  imageUrl: 'assets/images/luck.png',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Column(
+              children: [
+                CardBox(title: '자주가는목적지', height: 180),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            CardBox(
+              title: '최근 목적지',
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            CardBox(
+              title: '현재 위치 --동 장소 추천',
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CustomButton(
+                  text: '길찾기',
+                  borderColor: Color(0xFF7FC818),
+                  textColor: Color(0xFF7FC818),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
