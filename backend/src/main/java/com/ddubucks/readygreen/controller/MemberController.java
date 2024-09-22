71ab5b13e5311f002f42dd93eeb1c25394c8eeb2
qwsa522 <qwsa522@naver.com>
2024-09-22 14:26:11 +0900

    feat : root context path 수정 #349

--- backend/src/main/java/com/ddubucks/readygreen/controller/MemberController.java
+++ backend/src/main/java/com/ddubucks/readygreen/controller/MemberController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class MemberController {
 
     private final MemberService memberService;
