f5ea3ab93826762993ef6a4f0a5c0ff1f7493a67
qwsa522 <qwsa522@naver.com>
2024-09-23 14:49:26 +0900

    feat : health check 코드에 env 테스트 추가 #351

--- backend/src/main/java/com/ddubucks/readygreen/controller/HealthCheckController.java
+++ backend/src/main/java/com/ddubucks/readygreen/controller/HealthCheckController.java
@@ -2,6 +2,8 @@ package com.ddubucks.readygreen.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Health Check", description = "헬스 체크 코드")
 public class HealthCheckController {
 
+    private final Environment env;
+
+    @Autowired
+    public HealthCheckController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/")
     @Operation(summary = "Health Check", description = "헬스 체크")
     public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>("Server is running!", HttpStatus.OK);
+        // 예시로 DB_USERNAME 환경 변수를 가져와서 출력
+        String dbUsername = env.getProperty("DB_USERNAME");
+
+        String message = "Server is running! Connected as: " + dbUsername;
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
