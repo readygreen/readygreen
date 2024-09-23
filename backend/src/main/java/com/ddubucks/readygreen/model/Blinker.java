8441bdce47549e30a65c02e11bbc33cfb6401b95
서두나 <tjensk26@gmail.com>
2024-09-23 12:55:59 +0900

    feat: 신호등 정보 entity

---
+++ backend/src/main/java/com/ddubucks/readygreen/model/Blinker.java
@@ -0,0 +1,34 @@
+package com.ddubucks.readygreen.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "blinker")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Blinker extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private int greenDuration;
+
+    @Column(nullable = false)
+    private int redDuration;
+
+    @Column(nullable = false)
+    private Point coordinate;
+}
