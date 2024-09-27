079e3a0d025cc991fa1d8993ac07c87459c901bd
진아영 <ay8160@daum.net>
2024-09-27 16:00:50 +0900

    feat: 연결 화면 ui

--- wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/InitialScreen.kt
+++ wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/InitialScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Text
@@ -53,18 +55,24 @@ fun InitialScreen() {
         Spacer(modifier = Modifier.height(8.dp))
         TextField(
             value = text,
-            onValueChange = { newText -> text = newText },
+            onValueChange = { newText ->
+                if (newText.length <= 6 && newText.all { it.isDigit() }) {
+                    text = newText
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth(0.75f)
-                .padding(10.dp)
-                .height(40.dp)
+                .height(40.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            )
         )
         Spacer(modifier = Modifier.height(10.dp))
         TextButton(
             onClick = { /*TODO*/ },
             modifier = Modifier
                 .height(40.dp)
-                .fillMaxWidth(0.32f),
+                .fillMaxWidth(0.4f),
             shape = RoundedCornerShape(40.dp),
             colors = ButtonDefaults.textButtonColors(
                 containerColor = DarkGray,
