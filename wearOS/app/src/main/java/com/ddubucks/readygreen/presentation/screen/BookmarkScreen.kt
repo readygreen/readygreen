0e3c30f5d2e5889c9f0b85d12430b11a48015c6b
진아영 <ay8160@daum.net>
2024-10-05 20:56:02 +0900

    feat: 북마크 길안내 시작

--- wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/BookmarkScreen.kt
+++ wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/BookmarkScreen.kt
@@ -16,6 +16,8 @@ import androidx.wear.compose.material.items
 import com.ddubucks.readygreen.R
 import com.ddubucks.readygreen.data.model.ButtonIconModel
 import com.ddubucks.readygreen.presentation.components.ButtonIconItem
+import com.ddubucks.readygreen.presentation.components.ModalItem
+import com.ddubucks.readygreen.presentation.retrofit.bookmark.BookmarkResponse
 import com.ddubucks.readygreen.presentation.theme.Black
 import com.ddubucks.readygreen.presentation.theme.Yellow
 import com.ddubucks.readygreen.presentation.viewmodel.BookmarkViewModel
@@ -28,79 +30,98 @@ fun BookmarkScreen(
     bookmarkViewModel: BookmarkViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
     navigationViewModel: NavigationViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
     navController: NavHostController,
-
-    ) {
+) {
     val bookmarks by bookmarkViewModel.bookmark.collectAsState(initial = emptyList())
     val context = LocalContext.current
+    var showModal by remember { mutableStateOf(false) }
+    var selectedBookmark by remember { mutableStateOf<BookmarkResponse?>(null) }
 
     LaunchedEffect(Unit) {
         bookmarkViewModel.getBookmarks()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        ScalingLazyColumn {
-            item {
-                Text(
-                    text = "자주가는 목적지",
-                    style = h3Style,
-                    color = Yellow,
-                )
-
-            }
-            item {
-                Spacer(modifier = Modifier.height(10.dp))
-            }
-            if (bookmarks.isNotEmpty()) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Black),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ScalingLazyColumn {
                 item {
                     Text(
-                        text = "목적지를 선택해주세요.",
-                        style = pStyle,
-                        color = Color.White,
+                        text = "자주가는 목적지",
+                        style = h3Style,
+                        color = Yellow,
                     )
                 }
                 item {
                     Spacer(modifier = Modifier.height(10.dp))
                 }
-                items(bookmarks) { bookmark ->
-                    val iconResId = when (bookmark.name) {
-                        "회사" -> R.drawable.bookmark_office
-                        "집" -> R.drawable.bookmark_home
-                        else -> R.drawable.bookmark_default
+                if (bookmarks.isNotEmpty()) {
+                    item {
+                        Text(
+                            text = "목적지를 선택해주세요.",
+                            style = pStyle,
+                            color = Color.White,
+                        )
                     }
-                    ButtonIconItem(
-                        item = ButtonIconModel(
-                            icon = iconResId,
-                            label = bookmark.destinationName
-                        ),
-                        onClick = {
-                            Log.d("BookmarkScreen", "북마크 버튼 클릭: ${bookmark.destinationName}")
-                            navigationViewModel.startNavigation(
-                                context,
-                                bookmark.latitude,
-                                bookmark.longitude,
-                                bookmark.destinationName
-                            )
-                            navController.navigate("navigationScreen") {
-                                popUpTo("mainScreen") { inclusive = false }
-                            }
+                    item {
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
+                    items(bookmarks) { bookmark ->
+                        val iconResId = when (bookmark.name) {
+                            "회사" -> R.drawable.bookmark_office
+                            "집" -> R.drawable.bookmark_home
+                            else -> R.drawable.bookmark_default
                         }
-                    )
-                }
-            } else {
-                item {
-                    Text(
-                        text = "저장된 북마크가 없습니다.",
-                        style = pStyle,
-                        color = Color.White,
-                    )
+                        ButtonIconItem(
+                            item = ButtonIconModel(
+                                icon = iconResId,
+                                label = bookmark.destinationName
+                            ),
+                            onClick = {
+                                Log.d("BookmarkScreen", "북마크 버튼 클릭: ${bookmark.destinationName}")
+                                selectedBookmark = bookmark
+                                showModal = true  // 모달을 표시하도록 설정
+                            }
+                        )
+                    }
+                } else {
+                    item {
+                        Text(
+                            text = "저장된 북마크가 없습니다.",
+                            style = pStyle,
+                            color = Color.White,
+                        )
+                    }
                 }
             }
+        }
+
+        if (showModal && selectedBookmark != null) {
+            ModalItem(
+                title = "길안내 시작",
+                message = "${selectedBookmark?.destinationName}으로 길안내를 시작할까요?",
+                onConfirm = {
+                    showModal = false
+                    selectedBookmark?.let { bookmark ->
+                        navigationViewModel.startNavigation(
+                            context,
+                            bookmark.latitude,
+                            bookmark.longitude,
+                            bookmark.destinationName
+                        )
+                        navController.navigate("navigationScreen") {
+                            popUpTo("mainScreen") { inclusive = false }
+                        }
+                    }
+                },
+                onCancel = {
+                    showModal = false  // 모달 닫기
+                }
+            )
         }
     }
 }
