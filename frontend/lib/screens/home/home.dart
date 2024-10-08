97b90b3894db06d4aec235a0eabad4ccc835a157
이승민 <tmdals77117@gmail.com>
2024-10-08 12:50:05 +0900

    feat: home 최근 목적지 글자수 처리

--- frontend/lib/screens/home/home.dart
+++ frontend/lib/screens/home/home.dart
@@ -517,91 +517,88 @@ class _HomePageContentState extends State<HomePageContent> {
                 borderRadius: BorderRadius.circular(15),
               ),
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      '최근 목적지',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: AppColors.greytext,
-                      ),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '최근 목적지',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: AppColors.greytext,
                     ),
-                    routeRecords != null && routeRecords!.isNotEmpty
-                        ? Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '${routeRecords?['endName']}',
-                                      // '${formatDestinationName(routeRecords?['endName'])}',
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                      maxLines: 2, // 최대 2줄로 설정
-                                      overflow: TextOverflow
-                                          .ellipsis, // 넘치는 텍스트는 '...'로 표시
-                                    ),
-                                  ],
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    print(routeRecords?['endName']);
-                                    print(routeRecords?['endLatitude']);
-                                    print(routeRecords?['endLongitude']);
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => MapDirectionPage(
-                                          endLat: routeRecords?['endLatitude'],
-                                          endLng: routeRecords?['endLongitude'],
-                                          endPlaceName:
-                                              routeRecords?['endName'],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    foregroundColor: AppColors.green,
-                                    backgroundColor: Colors.white,
-                                    side: const BorderSide(
-                                      color: AppColors.green,
-                                      width: 1.0,
-                                    ),
-                                    elevation: 0,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
+                  ),
+                  routeRecords != null && routeRecords!.isNotEmpty
+                      ? Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // 텍스트를 Flexible로 감싸서 공간 확보
+                              Flexible(
+                                child: Text(
+                                  '${routeRecords?['endName']}',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  child: const Text(
-                                    '길찾기',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: AppColors.green,
+                                  maxLines: 1, // 최대 2줄로 설정
+                                  overflow: TextOverflow
+                                      .ellipsis, // 넘치는 텍스트는 '...'로 표시
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  print(routeRecords?['endName']);
+                                  print(routeRecords?['endLatitude']);
+                                  print(routeRecords?['endLongitude']);
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => MapDirectionPage(
+                                        endLat: routeRecords?['endLatitude'],
+                                        endLng: routeRecords?['endLongitude'],
+                                        endPlaceName: routeRecords?['endName'],
+                                      ),
                                     ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: AppColors.green,
+                                  backgroundColor: Colors.white,
+                                  side: const BorderSide(
+                                    color: AppColors.green,
+                                    width: 1.0,
+                                  ),
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
                                 ),
-                              ],
-                            ),
-                          )
-                        : Column(
-                            children: const [
-                              SizedBox(height: 10), // 여백 추가
-                              Text(
-                                '최근 목적지가 없습니다.',
-                                style: TextStyle(
-                                  fontSize: 18, // 글씨 크기
-                                  fontWeight: FontWeight.bold, // 글씨 굵기
+                                child: const Text(
+                                  '길찾기',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.green,
+                                  ),
                                 ),
                               ),
                             ],
-                          )
-                  ]),
+                          ),
+                        )
+                      : Column(
+                          children: const [
+                            SizedBox(height: 10), // 여백 추가
+                            Text(
+                              '최근 목적지가 없습니다.',
+                              style: TextStyle(
+                                fontSize: 18, // 글씨 크기
+                                fontWeight: FontWeight.bold, // 글씨 굵기
+                              ),
+                            ),
+                          ],
+                        )
+                ],
+              ),
             ),
 
             const SizedBox(height: 16),
