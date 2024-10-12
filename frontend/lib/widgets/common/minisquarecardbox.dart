907e286f4e9c0f5afa8000d13f7c777afc2d7c3a
이승민 <tmdals77117@gmail.com>
2024-10-12 22:56:06 +0900

    feat: 메인 ui 수정

--- frontend/lib/widgets/common/minisquarecardbox.dart
+++ frontend/lib/widgets/common/minisquarecardbox.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
-class SquareCardBox extends StatelessWidget {
+class MiniSquareCardBox extends StatelessWidget {
   final String title;
   final Color? backgroundColor;
   final Gradient? backgroundGradient;
   final Color textColor;
   final String? imageUrl;
-  final Color subtitleColor;
+  final String? subtitle; // 하단에 표시할 작은 텍스트 추가
+
   final Widget? child;
 
-  const SquareCardBox({
+  const MiniSquareCardBox({
     super.key,
     required this.title,
     this.backgroundColor = Colors.white,
     this.backgroundGradient,
     this.textColor = Colors.black,
     this.imageUrl,
-    this.subtitleColor = Colors.black,
+    this.subtitle, // subtitle 매개변수 추가
+
     this.child,
   });
 
@@ -25,8 +27,8 @@ class SquareCardBox extends StatelessWidget {
     final deviceWidth = MediaQuery.of(context).size.width;
 
     return Container(
-      width: deviceWidth / 3,
-      height: deviceWidth / 2.4,
+      width: deviceWidth / 3.5,
+      height: deviceWidth / 3.5,
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         gradient: backgroundGradient,
@@ -35,32 +37,27 @@ class SquareCardBox extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          vertical: 12,
-          horizontal: 16,
+          vertical: 7,
+          horizontal: 10,
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, // 아이템 중앙 정렬
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center, // 아이템을 수평 중앙 정렬
           children: [
             Text(
               title,
               style: TextStyle(
-                fontSize: 15,
-                // fontWeight: FontWeight.bold,
+                fontSize: 12,
                 color: textColor,
               ),
             ),
             if (imageUrl != null)
               Padding(
-                padding: const EdgeInsets.only(top: 1.2),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    imageUrl!,
-                    // width: deviceWidth / 4.6,
-                    height: deviceWidth / 3.5,
-                    fit: BoxFit.cover,
-                  ),
+                padding: const EdgeInsets.only(top: 7.0),
+                child: Image.asset(
+                  imageUrl!,
+                  height: deviceWidth / 6,
+                  fit: BoxFit.fitWidth,
                 ),
               ),
             if (child != null) child!,
