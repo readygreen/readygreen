74b7a060768dd3bad9f726e6bb08e71b723e8e05
진아영 <ay8160@daum.net>
2024-10-12 19:49:51 +0900

    fix: 길찾기 데이터 비동기

--- wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/BookmarkScreen.kt
+++ wearOS/app/src/main/java/com/ddubucks/readygreen/presentation/screen/BookmarkScreen.kt
@@ -1,7 +1,5 @@
 package com.ddubucks.readygreen.presentation.screen
 
-import android.speech.tts.TextToSpeech
-import android.speech.tts.UtteranceProgressListener
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -26,7 +24,6 @@ import com.ddubucks.readygreen.presentation.viewmodel.BookmarkViewModel
 import com.ddubucks.readygreen.presentation.viewmodel.NavigationViewModel
 import h3Style
 import pStyle
-import java.util.*
 
 @Composable
 fun BookmarkScreen(
@@ -38,87 +35,53 @@ fun BookmarkScreen(
     val context = LocalContext.current
     var showModal by remember { mutableStateOf(false) }
     var selectedBookmark by remember { mutableStateOf<BookmarkResponse?>(null) }
-
-    var ttsReady by remember { mutableStateOf(false) }
-    var tts by remember { mutableStateOf<TextToSpeech?>(null) }
-    var navigateAfterTTS by remember { mutableStateOf(false) }
-    var isSpeaking by remember { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(Unit) {
         bookmarkViewModel.getBookmarks()
+        isLoading = false
     }
 
-    LaunchedEffect(Unit) {
-        tts = TextToSpeech(context) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                tts?.language = Locale.KOREAN
-                ttsReady = true
-
-                tts?.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
-                    override fun onStart(utteranceId: String?) {
-                        isSpeaking = true
-                    }
-
-                    override fun onDone(utteranceId: String?) {
-                        isSpeaking = false
-                        navigateAfterTTS = true
-                    }
-
-                    override fun onError(utteranceId: String?) {
-                        isSpeaking = false
-                    }
-                })
+    // UI 구성
+    Box(modifier = Modifier.fillMaxSize()) {
+        when {
+            isLoading -> {
+                LoadingScreen()
             }
-        }
-    }
-
-    DisposableEffect(Unit) {
-        onDispose {
-            tts?.shutdown()
-        }
-    }
-
-    LaunchedEffect(navigateAfterTTS) {
-        if (navigateAfterTTS && selectedBookmark != null) {
-            val place = selectedBookmark
-            if (place != null) {
-                navigationViewModel.startNavigation(
-                    context,
-                    place.latitude,
-                    place.longitude,
-                    place.destinationName
-                )
-                navController.navigate("navigationScreen") {
-                    popUpTo("mainScreen") { inclusive = false }
+            bookmarks.isEmpty() -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Black),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "저장된 북마크가 없습니다.",
+                        style = pStyle,
+                        color = Color.White,
+                    )
                 }
-                navigateAfterTTS = false
             }
-        }
-    }
-
-    if (isSpeaking) {
-        LoadingScreen()
-    } else {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Black),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                ScalingLazyColumn {
-                    item {
-                        Text(
-                            text = "자주가는 목적지",
-                            style = h3Style,
-                            color = Primary,
-                        )
-                    }
-                    item {
-                        Spacer(modifier = Modifier.height(10.dp))
-                    }
-                    if (bookmarks.isNotEmpty()) {
+            else -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Black),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    ScalingLazyColumn {
+                        item {
+                            Text(
+                                text = "자주가는 목적지",
+                                style = h3Style,
+                                color = Primary,
+                            )
+                        }
+                        item {
+                            Spacer(modifier = Modifier.height(10.dp))
+                        }
                         item {
                             Text(
                                 text = "목적지를 선택해주세요.",
@@ -147,26 +110,25 @@ fun BookmarkScreen(
                                 }
                             )
                         }
-                    } else {
-                        item {
-                            Text(
-                                text = "저장된 북마크가 없습니다.",
-                                style = pStyle,
-                                color = Color.White,
-                            )
-                        }
                     }
                 }
             }
+        }
 
-            if (showModal && selectedBookmark != null) {
+        if (showModal && selectedBookmark != null) {
+            selectedBookmark?.let { bookmark ->
                 Modal(
                     title = "길안내 시작",
-                    message = "${selectedBookmark?.destinationName}으로 길안내를 시작할까요?",
+                    message = "${bookmark.destinationName}으로 길안내를 시작할까요?",
                     onConfirm = {
-                        val place = selectedBookmark
-                        if (place != null && ttsReady && place.destinationName != null) {
-                            tts?.speak("${place.destinationName}으로 길안내를 시작합니다", TextToSpeech.QUEUE_FLUSH, null, "ttsId")
+                        navigationViewModel.startNavigation(
+                            context,
+                            bookmark.latitude,
+                            bookmark.longitude,
+                            bookmark.destinationName
+                        )
+                        navController.navigate("navigationScreen") {
+                            popUpTo("mainScreen") { inclusive = false }
                         }
                         showModal = false
                     },
